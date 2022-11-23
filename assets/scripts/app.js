a25593928592e8fbe76ee6608838d09f094129bc
Michael Njuki <mikenjuki@gmail.com>
2022-11-23 18:25:37 +0300

    Separated components into classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,8 +7,33 @@ class Product {
     }
 }
 
-const productList = {
-    products: [
+
+class ProductItem {
+    constructor(product) {
+        this.product = product;
+    }
+
+    renderListItems() {
+        const prodEl = document.createElement("li");
+        prodEl.className = "product-item";
+        prodEl.innerHTML = `
+        <div>
+            <img src="${this.product.imageUrl} alt="${this.product.title}" >
+            <div class="product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>&dollar;${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+        `;
+        return prodEl;
+    }
+}
+
+
+class ProductList {
+    products = [
         new Product(
             "Fresh Kisses",
             "https://images.unsplash.com/photo-1604544203292-0daa7f847478?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGRvZyUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
@@ -32,33 +57,28 @@ const productList = {
             "https://images.unsplash.com/photo-1535294435445-d7249524ef2e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
             5.39,
             "Kong bone dog toy."
-        )
-    ],
+        )];
+
+    constructor() {};
+
     render() {
+
         const renderHook = document.getElementById("app");
         const prodList = document.createElement("ul");
         prodList.className = "product-list";
         for (const prod of this.products) {
-            const prodEl = document.createElement("li");
-            prodEl.className = "product-item";
-            prodEl.innerHTML = `
-            <div>
-                <img src="${prod.imageUrl} alt="${prod.title}" >
-                <div class="product-item__content">
-                    <h2>${prod.title}</h2>
-                    <h3>&dollar;${prod.price}</h3>
-                    <p>${prod.description}</p>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.renderListItems();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
-    }
 
-};
+    };
+
+}
 
 
+
+const productList = new ProductList();
 productList.render();
 
